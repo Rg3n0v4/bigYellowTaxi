9691b9eba5926659b8cf88ec39938825008bf741
kaowayne8 <wkao9@uic.edu>
2022-04-21 23:48:38 -0500

    Fixed graph

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -6,6 +6,12 @@ myfiles
 data <- do.call(rbind, lapply(myfiles, read.csv, header = FALSE))
 colnames(data) <- col_name
 
+#ADDED
+n_date <- parse_date_time(data$Trip.Start.Timestamp,
+                          orders = 'mdY IMS %p', truncated = 3) #PARSE DATE FROM TIME STAMP
+data$new_date <- n_date
+data$Hour <- hour(data$new_date)
+
 #libraries to include
 
 library(shiny)
@@ -21,7 +27,7 @@ library(gridExtra)
 library(dplyr)
 library(rlang)
 
-community_list <- c("Rogers Park", "West Ridge", "Uptown", 
+community_list <- c("Rogers Park", "West Ridge", "Uptown",
                     "Lincoln Square", "North Center", "Lake View",
                     "Lincoln Park", "Near North Side", "Edison Park",
                     "Norwood Park", "Jefferson Park", "Forest Glen",
@@ -31,7 +37,7 @@ community_list <- c("Rogers Park", "West Ridge", "Uptown",
                     "West Town", "Austin", "West Garfield Park",
                     "East Garfield Park", "Near West Side", "North Lawndale",
                     "South Lawndale", "Lower West Side", "Loop", "Near South Side",
-                    "Armour Square", "Douglas", "Oakland", "Fuller Park", 
+                    "Armour Square", "Douglas", "Oakland", "Fuller Park",
                     "Grand Boulevard", "Kenwood", "Washington Park", "Hyde Park",
                     "Woodlawn", "South Shore", "Chatham", "Avalon Park", "South Chicago",
                     "Burnside", "Calumet Heights", "Roseland", "Pullman", "South Deering",
@@ -45,20 +51,20 @@ sorted_community_list <- sort(community_list)
 new_sorted_community_list <- append(sorted_community_list, "All (City of Chicago)", 0)
 
 company_list <- c("1085 - 72312 N and W Cab Co", "1469 - 64126 Omar Jada","2092 - 61288 Sbeih company", "24 Seven Taxi",
-                  "2733 - 74600 Benny Jona", "3011 - 66308 JBL Cab Inc.", "3094 - 24059 G.L.B. Cab Co", 
-                  "312 Medallion Management Corp", "3556 - 36214 RC Andrews Cab", "3591 - 63480 Chuks Cab", 
-                  "3620 - 52292 David K. Cab Corp.", "3623 - 72222 Arrington Enterprises", 
-                  "3721 - Santamaria Express, Alvaro Santamaria", "4053 - 40193 Adwar H. Nikola", 
+                  "2733 - 74600 Benny Jona", "3011 - 66308 JBL Cab Inc.", "3094 - 24059 G.L.B. Cab Co",
+                  "312 Medallion Management Corp", "3556 - 36214 RC Andrews Cab", "3591 - 63480 Chuks Cab",
+                  "3620 - 52292 David K. Cab Corp.", "3623 - 72222 Arrington Enterprises",
+                  "3721 - Santamaria Express, Alvaro Santamaria", "4053 - 40193 Adwar H. Nikola",
                   "4623 - 27290 Jay Kim", "5 Star Taxi", "5006 - 39261 Salifu Bawa", "5062 - 34841 Sam Mestas",
                   "5074 - 54002 Ahzmi Inc", "5874 - 73628 Sergey Cab Corp.", "6574 - Babylon Express Inc.",
-                  "6742 - 83735 Tasha ride inc", "6743 - 78771 Luhak Corp", "American United", 
+                  "6742 - 83735 Tasha ride inc", "6743 - 78771 Luhak Corp", "American United",
                   "American United Taxi Affiliation", "Blue Diamond", "Blue Ribbon Taxi Association Inc.",
-                  "Checker Taxi", "Checker Taxi Affiliation", "Chicago Carriage Cab Corp", 
-                  "Chicago Independents", "Chicago Medallion Management", "Chicago Star Taxicab", 
-                  "Chicago Taxicab", "Choice Taxi Association", "City Service", "Flash Cab", "Globe Taxi", 
-                  "Gold Coast Taxi", "KOAM Taxi Association", "Leonard Cab Co", "Medallion Leasin", 
-                  "Metro Jet Taxi A", "Nova Taxi Affiliation Llc", "Patriot Taxi Dba Peace Taxi Associat", 
-                  "Setare Inc", "Star North Management LLC", "Sun Taxi", "Taxi Affiliation Service Yellow", 
+                  "Checker Taxi", "Checker Taxi Affiliation", "Chicago Carriage Cab Corp",
+                  "Chicago Independents", "Chicago Medallion Management", "Chicago Star Taxicab",
+                  "Chicago Taxicab", "Choice Taxi Association", "City Service", "Flash Cab", "Globe Taxi",
+                  "Gold Coast Taxi", "KOAM Taxi Association", "Leonard Cab Co", "Medallion Leasin",
+                  "Metro Jet Taxi A", "Nova Taxi Affiliation Llc", "Patriot Taxi Dba Peace Taxi Associat",
+                  "Setare Inc", "Star North Management LLC", "Sun Taxi", "Taxi Affiliation Service Yellow",
                   "Taxi Affiliation Services", "Taxicab Insurance Agency, LLC", "Top Cab Affiliation", "Yellow Cab",
                   "Petani Cab Corp", "U Taxicab")
 
@@ -89,11 +95,11 @@ ui <- shinyUI(
                                               br(),br(),br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
                                               br(),br(),br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
                                               selectInput("select_community", "Select Community", new_sorted_community_list, selected="All (City of Chicago)"),
-                                              
+
                                               selectInput("select_company", "Select Company", new_company_list, selected = "All Taxis"),
-                                              
+
                                               selectInput("select_distribution", "Select Distribution Type", distributionType, selected = "By Day"),
-                                              
+
                                               radioButtons("measurement", "Measurement:",
                                                            c("Kilometer (km)" = "km",
                                                              "Miles (mi)" = "miles")
@@ -114,14 +120,14 @@ ui <- shinyUI(
                                                box(
                                                    title = "Bar Chart (for different scopes of the day)",solidHeader = TRUE, status = "primary", width = "100%", height = 750,
                                                    plotOutput("scopechart", width = "100%", height = 700)
-                                               )     
+                                               )
                                         ),
                                         column(2,id="table_box",
                                                box(
                                                   title = "Table ", width = "20%", height = 700
                                                 )
                                         )
-                                    ), br(), 
+                                    ), br(),
                                     fluidRow(id="top-row",
                                              column(9, id="communities_graph",
                                                     box(
@@ -135,7 +141,7 @@ ui <- shinyUI(
                                              )
                                     )
                                 )
-                                
+
                       )
              ),
              tabPanel("About",
@@ -165,26 +171,26 @@ server <- function(input, output, session) {
   company <- "All Taxi"
   units <- "MI"
   distribution <- "All" # intput$select_distribution
-  
+
   graph_scope_chart <- function(){
-    
+
     # d_graph <- allStops1()
     g <- NULL
     # i_order = orderOpt2()
-    
-    
-    
+
+
+
     return(g)
   }
-  
-  output$scopechart <- renderPlot({ 
+
+  output$scopechart <- renderPlot({
     noFilter_byDay <- aggregate(data[,1], by=list(date(data$new_date)), FUN=length)
     colnames(noFilter_byDay) <- c("Date", "Rides")
-    
-    ggplot(data=noFilter_byDay, aes(x=noFilter_byDay$Date, y=noFilter_byDay$Rides)) + geom_bar(stat="identity") 
-    
+
+    ggplot(data=noFilter_byDay, aes(x=noFilter_byDay$Date, y=noFilter_byDay$Rides)) + geom_bar(stat="identity")
+
     })
-  
+
 }
 
 shinyApp(ui = ui, server = server)
